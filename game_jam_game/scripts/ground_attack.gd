b6ab2c8daff283ae3ec8ef5b4233b73f473a992c
Joey Hersh <joeyandpaige@gmail.com>
2025-08-01 18:11:15 -0500

    added some collision logic af fixed some indention issues

--- game_jam_game/scripts/ground_attack.gd
+++ game_jam_game/scripts/ground_attack.gd
@@ -12,52 +12,40 @@ extends State
 var sword_anim: AnimationPlayer
 var hitbox: HitBox
 
-
-
 func enter() -> void:
-        super()
-        hitbox = parent.get_node("AnimatedSprite2D/Sword/Sprite2D/HitBox") as HitBox
+	super()
+	hitbox = parent.get_node("AnimatedSprite2D/Sword/Sprite2D/HitBox") as HitBox
 
 func process_input(_event: InputEvent) -> State:
 	sword_anim = parent.get_node("AnimatedSprite2D/Sword/AnimationPlayer") as AnimationPlayer
-	
-		# … then look at what the player is holding **right now**.
+
+	# … then look at what the player is holding **right now**.
 	if Input.is_action_pressed("up"):
 		sword_anim.play("up_ward_swing")
-                hitbox.attack_dir = "up"
+		hitbox.attack_dir = "up"
 		print("Playing Upward Swing Animation")
 		return idle_state
-	elif Input.is_action_pressed("crouch"):	# or "down"
+	elif Input.is_action_pressed("crouch"): # or "down"
 		sword_anim.play("down_ward_swing")
-                hitbox.attack_dir = "down"
+		hitbox.attack_dir = "down"
 		print("Playing Downward Swing Animation")
 		return crouch_state
 	else:
 		sword_anim.play("swing")
-                hitbox.attack_dir = "side"
+		hitbox.attack_dir = "side"
 		print("Playing Swing Animation")
 		return idle_state
 
 	return null
-		
-		 
-
 
 func process_physics(delta: float) -> State:
 	parent.velocity.x = move_toward(parent.velocity.x, 0.0, deceleration * delta)
 	parent.move_and_slide()
 	return null
 
-
-	# for example: if the player is holding left/right, go back to Move, else Idle
-	
-		
-		
-
-
 #func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	#print("Animation Finished, listening for other states")
-	#if Input.get_axis("move_left", "move_right") != 0:
-		#parent.state_machine.change_state(move_state)
-	#else:
-		#parent.state_machine.change_state(idle_state)
+#	print("Animation Finished, listening for other states")
+#	if Input.get_axis("move_left", "move_right") != 0:
+#		parent.state_machine.change_state(move_state)
+#	else:
+#		parent.state_machine.change_state(idle_state)
--- game_jam_game/scripts/hit_hurt_boxes/hurtbox.gd
+++ game_jam_game/scripts/hit_hurt_boxes/hurtbox.gd
@@ -2,7 +2,6 @@ class_name HurtBox
 
 extends Area2D
 
-
 func _init() -> void:
 	collision_layer = 0
 	collision_mask = 4
@@ -13,25 +12,25 @@ func _ready() -> void:
 func _on_area_entered(hitbox: HitBox) -> void:
 	if hitbox == null:
 		return
-	
+
 	if owner.has_method("take_damage"):
 		owner.take_damage(hitbox.damage)
-		
-       if owner.has_method("apply_knockback"):
-               var horiz_dir := sign(owner.global_position.x - hitbox.global_position.x)
-               if horiz_dir == 0:
-                       horiz_dir = 1 # default knock right if overlapping
 
-               var dir: Vector2
-               match hitbox.attack_dir:
-                       "up":
-                               dir = Vector2(horiz_dir * 0.2, -1)
-                       "down":
-                               dir = Vector2(horiz_dir * 0.2, 1)
-                       "side":
-                               dir = Vector2(horiz_dir, -0.2)
-                       _:
-                               dir = Vector2(horiz_dir, -0.5)
+	if owner.has_method("apply_knockback"):
+		var horiz_dir: int = sign(owner.global_position.x - hitbox.global_position.x)
+		if horiz_dir == 0:
+			horiz_dir = 1 # default knock right if overlapping
 
-               dir = dir.normalized()
-               owner.apply_knockback(dir * hitbox.knockback_multiplier)
+		var dir: Vector2
+		match hitbox.attack_dir:
+			"up":
+				dir = Vector2(horiz_dir * 0.2, -1)
+			"down":
+				dir = Vector2(horiz_dir * 0.2, 1)
+			"side":
+				dir = Vector2(horiz_dir, -0.2)
+			_:
+				dir = Vector2(horiz_dir, -0.5)
+
+		dir = dir.normalized()
+		owner.apply_knockback(dir * hitbox.knockback_multiplier)
